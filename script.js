73c20edef973d07ad0e60d1338e25b9d3a62a2e7
Nimira43 <n.rabone72@aol.com>
2024-08-25 21:19:33 +0100

    Created Effect and Particle classes

--- script.js
+++ script.js
@@ -5,10 +5,26 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 ctx.fillStyle = 'white'
 ctx.strokeStyle = 'white'
-ctx.lineWidth = 100
-ctx.lineCap = 'round'
+ctx.lineWidth = 1
 
-ctx.beginPath()
-ctx.moveTo(100, 200)
-ctx.lineTo(400, 500)
-ctx.stroke()
+class Particle {
+  constructor(effect) {
+    this.effect = effect
+    this.x = Math.floor(Math.random() * this.effect.width)
+    this.y = Math.floor(Math.random() * this.effect.height)
+  }
+  draw(context) {
+    context.fillRect(this.x, this.y, 10, 10)
+  }
+}
+
+class Effect {
+  constructor(width, height) {
+    this.width = width
+    this.height = height
+    this.particles = []
+  }
+  init() {
+    this.particles.push(new Particle())
+  }
+}
