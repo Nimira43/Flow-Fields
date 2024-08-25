39dbf6a0ad17a223ed726e7f36f9184cfe46f63b
Nimira43 <n.rabone72@aol.com>
2024-08-25 23:58:14 +0100

    Updated

--- script.js
+++ script.js
@@ -12,10 +12,11 @@ class Particle {
     this.effect = effect
     this.x = Math.floor(Math.random() * this.effect.width)
     this.y = Math.floor(Math.random() * this.effect.height)
-    this.speedX = Math.random() * 5 - 2.5
-    this.speedY = Math.random() * 5 - 2.5
+    this.speedX 
+    this.speedY 
+    this.speedModifier = Math.floor(Math.random() * 5 + 1)
     this.history = [{ x: this.x, y: this.y }]
-    this.maxLength = Math.floor(Math.random() * 100 + 10)
+    this.maxLength = Math.floor(Math.random() * 200 + 10)
     this.angle = 0
   }
   draw(context) {
@@ -33,12 +34,18 @@ class Particle {
     this.angle = this.effect.flowField[index]
     this.speedX = Math.cos(this.angle)
     this.speedY = Math.sin(this.angle)
-    this.x += this.speedX
-    this.y += this.speedY
+    this.x += this.speedX * this.speedModifier
+    this.y += this.speedY * this.speedModifier
     this.history.push({ x: this.x, y: this.y })
     if (this.history.length > this.maxLength) {
       this.history.shift()
     }
+  }
+  reset() {
+    this.x = Math.floor(Math.random() * this.effect.width)
+    this.y = Math.floor(Math.random() * this.effect.height)
+    this.history = [{ x: this.x, y: this.y }]
+    
   }
 }
 
@@ -47,11 +54,13 @@ class Effect {
     this.width = width
     this.height = height
     this.particles = []
-    this.numberOfParticles = 50
+    this.numberOfParticles = 300
     this.cellSize = 20
     this.rows
     this.cols
     this.flowField = []
+    this.curve = 0.5
+    this.zoom = 0.13
     this.init()
   }
   init() {
@@ -60,10 +69,9 @@ class Effect {
     this.flowField = []
     for (let y = 0; y < this.rows; y++) {
       for (let x = 0; x < this.cols; x++) {
-        let angle = (Math.cos(x) + Math.sin(y)) * 2
+        let angle = (Math.cos(x * this.zoom) + Math.sin(y * this.zoom)) * this.curve
         this.flowField.push(angle)
       }
-      console.log(this.flowField)
     }
     for (let i = 0; i < this.numberOfParticles; i++) {
       this.particles.push(new Particle(this))
