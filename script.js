906ad93dd8feb5f3706b86b930e8d4a5891f18c3
Nimira43 <n.rabone72@aol.com>
2024-08-25 20:56:18 +0100

    SEt upp canvas and first shape.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-import * as THREE from 'three'
-console.log(THREE)
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
 
-const scene
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 100
+ctx.lineCap = 'round'
+
+ctx.beginPath()
+ctx.moveTo(100, 200)
+ctx.lineTo(400, 500)
+ctx.stroke()
